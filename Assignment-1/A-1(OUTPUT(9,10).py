f125d615e9e43a94c2e905babc914e51d1754ec1
adityasonu2006 <24cse118.adityapatra@giet.edu>
2026-08-04 11:11:00 +0530

    Add files via upload

---
+++ Assignment-1/A-1(OUTPUT(9,10).py
@@ -0,0 +1,122 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:\Users\Asus\OneDrive\Desktop\bubu\DMDW LAB ASSIGNMENT\Assignment-1\A-1,Q-7.py
+enter teh count5
+0
+1
+1
+2
+3
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-9.py
+enter the product number:100
+enter the name:jash
+enter the cost:1000
+enter the qty:2
+enter the product number:101
+enter the name:jash2
+enter the cost:20
+enter the qty:4
+enter the product number:102
+enter the name:jash3
+enter the cost:234
+enter the qty:6
+enter the product number:103
+enter the name:jash8
+enter the cost:23456
+enter the qty:1
+enter the product number:104
+enter the name:jash23
+enter the cost:34
+enter the qty:16
+
+ Highest cost:
+Traceback (most recent call last):
+  File "C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-9.py", line 28, in <module>
+    high.diplay()
+AttributeError: 'Product' object has no attribute 'diplay'. Did you mean: 'display'?
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-9.py
+enter the product number:1
+enter the name:k
+enter the cost:8
+enter the qty:8
+enter the product number:2
+enter the name:h
+enter the cost:8
+enter the qty:9
+enter the product number:3
+enter the name:f
+enter the cost:6
+enter the qty:7
+enter the product number:4
+enter the name:h
+enter the cost:7
+enter the qty:8
+enter the product number:5
+enter the name:y
+enter the cost:7
+enter the qty:8
+
+ Highest cost:
+product_no- 1
+product_name- k
+cost- 8
+quantity- 8
+Total_amount- 64
+product_no- 1
+product_name- k
+cost- 8
+quantity- 8
+Total_amount- 64
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py
+enter the id:1
+enter the name:jashobanta
+enter the basic_pay:20000
+enter the ta:2000
+enter the da:1000
+Traceback (most recent call last):
+  File "C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py", line 18, in <module>
+    p.clac()
+AttributeError: 'Employee' object has no attribute 'clac'
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py
+enter the id:1
+enter the name:jashobanta
+enter the basic_pay:20000
+enter the ta:1000
+enter the da:200
+Traceback (most recent call last):
+  File "C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py", line 19, in <module>
+    p.output()
+  File "C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py", line 11, in output
+    print("emp_id-",self.product_no)
+AttributeError: 'Employee' object has no attribute 'product_no'
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py
+enter the id:1
+enter the name:jashobanta 20000
+enter the basic_pay:1990
+enter the ta:1000
+enter the da:2000
+emp_id- 1
+emp_name- jashobanta 20000
+basic_pay- 1990
+ta- 1000
+da- 2000
+gross_pay- 2890.0
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-10.py
+enter the id:1
+enter the name:jashobanta sasmal
+enter the basic_pay:200000
+enter the ta:2000
+enter the da:1000
+emp_id- 1
+emp_name- jashobanta sasmal
+basic_pay- 200000
+ta- 2000
+da- 1000
+gross_pay- 200600.0
---
+++ Assignment-1/A-1(OUTPUT).py
@@ -0,0 +1,51 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-1.py
+enteh the string:virat
+Actual string: virat
+Reversed string: tariv
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-2.py
+(10, 20, 30, 40, 50)
+<class 'tuple'>
+10
+20
+30
+40
+50
+
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-3.py
+{'apple': 1, 'banana': 2, 'orange': 3}
+<class 'dict'>
+apple : 1
+banana : 2
+orange : 3
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-4.py
+{1, 2, 3}
+<class 'set'>
+1
+2
+3
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-5.py
+enter value for a:10
+enter value for b:20
+enter value for c:30
+30 is the greatest
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-6.py
+enter the number13
+it is a prime number
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-7.py
+enter teh count5
+0
+1
+1
+2
+3
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-1/A-1,Q-8.py
+content copied succesfully
